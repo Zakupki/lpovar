c8e14cc3f7b2540efc83226db0fb4be7132776a0
orange22 <dmitriy.bozhok@gmail.com>
2014-01-27 08:58:18 +0200

    234

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -5,7 +5,7 @@ $config = array(
             'class' => 'system.gii.GiiModule',
             'password' => 'bozhok1984',
             // If removed, Gii defaults to localhost only. Edit carefully to taste.
-            'ipFilters' => array('127.0.0.1', '31.42.52.10', '91.209.51.157', '::1'),
+            'ipFilters' => array('127.0.0.1', '31.42.52.10', '91.209.51.157', '195.177.72.222', '::1'),
             'generatorPaths' => array(
                 'backend.gii',
                 'backend.extensions.bootstrap.gii',
