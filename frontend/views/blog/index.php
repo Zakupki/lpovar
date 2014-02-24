c70960a6106b9db0f1551b3670c402054468a506
orange22 <dmitriy.bozhok@gmail.com>
2014-02-25 00:15:38 +0200

    products

--- frontend/views/blog/index.php
+++ frontend/views/blog/index.php
@@ -5,7 +5,7 @@
     </div>
 </div>
 <div id="blog"><!--main start-->
-    <div class="blogger">
+    <!--<div class="blogger">
         <div class="blogger-logo">
             <div class="blogger-round"></div>
             <?=$items[0]->user->image->asHtmlImage();?>
@@ -15,7 +15,7 @@
             <h2>Дмитрий Божок</h2>
             <p>Лазанья - национальное блюдо итальянской кухни. Тесто для неё готовится исключительно из твердых сортов пшениц. Правильное блюдо должно состоять как минимум из 4 слоёв теста с чередованием мясного соуса и соуса Бешамель. Лазанья - сочная и сытная, поэтому идеальным сочетанием к ней является витаминный салатный микс с помидорами черри и пармезаном...</p>
         </div>
-    </div>
+    </div>-->
     <div id="content">
         <div class="content-box">
             <ul class="lesson-list">
@@ -50,23 +50,29 @@
         <ul class="catalog-list">
             <? foreach($dishes as $dish){?>
             <li>
+                <? if($dish->new){?>
+                    <span class="news-label">Новинка</span>
+                <?}?>
                 <div class="img-holder">
-                    <a href="/product/view/133?title=blyudo_ot_komyuniti_shefa_aleny_mischenko_spagetti_karbonara">
-                        <img src="/upload/dishimage/5d/52453e50df14c2d041f6863fcf461a92.jpg" width="275" alt="Блюдо от комьюнити-шефа Алены Мищенко. Спагетти Карбонара">
+                    <a href="<?=$dish->getUrl();?>">
+                        <? if(isset($dish->dishImages[1]->image)){?>
+                        <img src="/<?=$dish->dishImages[1]->image->path.'/'.$dish->dishImages[1]->image->file;?>" width="275" alt="Блюдо от комьюнити-шефа Алены Мищенко. Спагетти Карбонара">
+                        <?}?>
                         <span class="mask">&nbsp;</span>
                     </a>
-                    <a data-cat="13" rel="2" href="/cart/add/133/?q=2" class="to-cart callbuy-popup"></a>
-                    <a href="/product/view/133?title=blyudo_ot_komyuniti_shefa_aleny_mischenko_spagetti_karbonara" class="play"></a>
+                    <? if(Option::getOpt('buy')){?>
+                        <a data-cat="<?=$dish->dishtype_id;?>" rel="<?=(isset($dish->portions[0]->value))?$dish->portions[0]->value:1;?>" href="/cart/add/<?=$dish->id;?>/?q=<?=(isset($dish->portions[0]->value))?$dish->portions[0]->value:1;?>" class="to-cart callbuy-popup"></a>
+                    <?}?>
+                    <a href="<?=$dish->getUrl();?>" class="play"></a>
                 </div>
                 <div class="info-row">
                     <span class="bottom-deco">&nbsp;</span>
                     <div class="info-frame">
-                        <div class="title">
-                            <a href="/product/view/133?title=blyudo_ot_komyuniti_shefa_aleny_mischenko_spagetti_karbonara">
-                                Блюдо от комьюнити-шефа Алены Мищенко...								</a>
-                        </div>
-                        <span class="price"><strong>59.00</strong> грн.</span>
-                        <span class="weight-text"><strong>250</strong> грамм</span>
+                        <div class="title"><a href="<?=$dish->getUrl();?>"><?=$dish->title;?></a></div>
+                        <span class="price"><strong><?=$dish->price;?></strong> грн.</span>
+                        <? if($dish->weight>0){?>
+                            <span class="weight-text"><strong><?=$dish->weight*1000;?></strong> грамм</span>
+                        <?}?>
                     </div>
                 </div>
             </li>
